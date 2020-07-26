9da079a8027fa7e9a07809e21f12faa20c2f4929
GBoz <47922128+CrusadorBoz@users.noreply.github.com>
2020-07-26 08:59:40 -0500

    Update TestBowlingGame.py

--- TestBowlingGame.py
+++ TestBowlingGame.py
@@ -19,7 +19,7 @@ class TestBowlingGame(unittest.TestCase):
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
-        self.rollMany(0,17)
+        self.game.rollMany(0,17)
         assert self.game.score() == 16
     #
     # def test_OneStrike(self):
@@ -36,6 +36,6 @@ class TestBowlingGame(unittest.TestCase):
     #     self.rollMany(5,21)
     #     assert self.game.score() == 150
 
-    def rollMany(self, pins, rolls):
-        for i in range(rolls):
-            self.game.roll(pins)
+    # def rollMany(self, pins, rolls):
+    #     for i in range(rolls):
+    #         self.game.roll(pins)
